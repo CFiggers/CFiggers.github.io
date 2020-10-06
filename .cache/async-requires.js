ee3a5ed8ca6311141dfc3fbdabaaa9183329390b
OverWilliam <cdfiggers@gmail.com>
2020-10-06 16:19:46 -0500

    Updated root dir to make sense

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,6 +2,7 @@
 const preferDefault = m => (m && m.default) || m
 
 exports.components = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---node-modules-gatsby-theme-andy-src-templates-note-js": () => import("./../../../node_modules/gatsby-theme-andy/src/templates/note.js" /* webpackChunkName: "component---node-modules-gatsby-theme-andy-src-templates-note-js" */)
 }
 
--- src/gatsby-theme-andy/components/Header.js
+++ src/gatsby-theme-andy/components/Header.js
@@ -6,7 +6,7 @@ import { Box, jsx } from 'theme-ui';
 export default ({ siteMetadata }) => (
   <header>
     <Box py={2} px={3} sx={{ borderBottom: '1px solid', borderColor: 'gray' }}>
-      <Link to="/home" sx={{ fontWeight: 'bold', color: 'text', textDecoration: 'none' }}>
+      <Link to="/" sx={{ fontWeight: 'bold', color: 'text', textDecoration: 'none' }}>
         {siteMetadata.title}
       </Link>
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
